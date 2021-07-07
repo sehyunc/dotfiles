cda3614a5e125c478a8f9ba127325303cb54e312
Sehyun Chung <sehyun@berkeley.edu>
2021-07-07 16:47:58 -0400

    run remote

--- .getdotfiles.sh
+++ .getdotfiles.sh
@@ -1,8 +1,9 @@
+# curl https://raw.githubusercontent.com/sehyunc/dotfiles/master/.getdotfiles.sh | bash
+
 echo "cloning dotfiles"
 git clone git@github.com:sehyunc/dotfiles.git "${HOME}/dotfiles"
-ln -s "${HOME}/dotfiles/.zshrc" "${HOME}/.zshrc"
-ln -s "${HOME}/dotfiles/.gitignore_global" "${HOME}/.gitignore_global"
 ln -s "${HOME}/dotfiles/.gitconfig" "${HOME}/.gitconfig"
-ln -s "${HOME}/dotfiles/.my_bin" "${HOME}/.my_bin"
+ln -s "${HOME}/dotfiles/.gitignore_global" "${HOME}/.gitignore_global"
+ln -s "${HOME}/dotfiles/.hyper.js" "${HOME}/.hyper.js"
 ln -s "${HOME}/dotfiles/.vimrc" "${HOME}/.vimrc"
-ln -s "${HOME}/dotfiles/.vimrc-parts" "${HOME}/.vimrc-parts"
+ln -s "${HOME}/dotfiles/.zshrc" "${HOME}/.zshrc"
